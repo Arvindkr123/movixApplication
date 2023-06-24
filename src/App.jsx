8784c96fa6ba51ce06b6cae8349db7da4c677624
Arvind <thakurarvindkr10@gmail.com>
2023-06-24 23:15:26 +0530

    Homepage - Hero Banner Section CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,44 @@ import { fetchDataFromApi } from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration } from './store/HomeSlice';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import Pages404 from './pages/404/404';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
 
 const App = () => {
     const dispatch = useDispatch();
     const { url } = useSelector((state) => state.home);
     console.log(url)
     useEffect(() => {
-        apiTesting();
+        fetchApiConfig();
     }, []);
 
-    const apiTesting = async () => {
-        const res = await fetchDataFromApi('/movie/popular');
-        dispatch(getApiConfiguration(res));
-        console.log(res);
+    const fetchApiConfig = async () => {
+        const res = await fetchDataFromApi('/configuration');
+        const url = {
+            backdrop: res.images.secure_base_url + "original",
+            poster: res.images.secure_base_url + "original",
+            profile: res.images.secure_base_url + "original",
+        }
+        dispatch(getApiConfiguration(url));
+        console.log('url', url)
     };
     return (
         <BrowserRouter>
+            {/* <Header/> */}
             <Routes>
-                <Route></Route>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/:mediaType/:id' element={<Details />}></Route>
+                <Route path='/search/:query' element={<SearchResult />}></Route>
+                <Route path='/explore/:mediaType' element={<Explore />}></Route>
+                <Route path='*' element={<Pages404 />}></Route>
             </Routes>
+            {/* <Footer/> */}
         </BrowserRouter>
     );
 };
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './style.scss'
+import useFetch from '../../../hooks/useFetch';
+import { useSelector } from 'react-redux';
+import Img from '../../../components/lazyloadimages/img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+
 const HeroBanner = () => {
+    const navigate = useNavigate();
+    const [background, setBackground] = useState('')
+    const [query, setQuery] = useState('')
+    const { data, loading } = useFetch("/movie/upcoming")
+    console.log('from hero banner', data)
+    const { url } = useSelector(state => state.home)
+
+    useEffect(() => {
+        const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg)
+    }, [data])
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`)
+            setQuery("")
+        }
+    }
+
     return (
-        <div>
-            Hero Banners 
+        <div className="heroBanner">
+            {!loading && <div className="backdrop-img">
+                <Img src={background} />
+            </div>}
+            <div className="opacity-layer">
+                
+            </div>
+            <ContentWrapper>
+                <div className="heroBannerContent">
+                    <span className="title">Welcome.</span>
+                    <span className="subtitle"> Millions of movies, TV shows and people to discover.Explore now.</span>
+                    <div className="searchInput">
+                        <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" placeholder='search for movie or tv show' onKeyUp={searchQueryHandler} />
+                        <button>Search</button>
+                    </div>
+
+                </div>
+            </ContentWrapper>
         </div>
     )
 }
