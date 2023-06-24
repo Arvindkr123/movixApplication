78f5f19cbec7d4a57a01c03af85c17d7c733e8c1
Arvind <thakurarvindkr10@gmail.com>
2023-06-24 08:41:15 +0530

    simple testing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react'
-import { fetchDataFromApi } from './utils/api'
+import React, { useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration } from './store/HomeSlice';
 
 const App = () => {
-     useEffect(() => {
-          apiTesting();
-     }, [])
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home);
+  console.log(url)
+  useEffect(() => {
+    apiTesting();
+  }, []);
 
-     const apiTesting = async () => {
-          const res = await fetchDataFromApi("/movie/popular")
-          console.log(res)
-     }
-     return (
-          <div>
-          </div>
-     )
-}
+  const apiTesting = async () => {
+    const res = await fetchDataFromApi('/movie/popular');
+    dispatch(getApiConfiguration(res));
+    console.log(res);
+  };
+     return <div>
+       {url?.total_pages}
+  </div>;
+};
 
-export default App
+export default App;
